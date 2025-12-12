b14b015344f31ae82ea1f74d24195471d7cbbc08
BlueGeckoJP <kei09doya@gmail.com>
2025-12-12 10:11:35 +0900

    feat: add output directory preparation and update filename path

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
+mod pattern;
+
+use std::{fs, path::Path};
+
 use image::ImageBuffer;
 
 use crate::pattern::{Pattern, checker};
 
-mod pattern;
+const OUTPUT_DIR: &str = "output";
+
+fn prepare_output_directory() {
+    let output_dir = Path::new(OUTPUT_DIR);
+    if !output_dir.exists() {
+        fs::create_dir(output_dir).expect("Failed to create output directory");
+    }
+}
 
 fn main() {
+    prepare_output_directory();
+
     let patterns: Vec<Box<dyn Pattern>> = vec![Box::new(checker::CheckerPattern {})];
 
     for pattern in patterns {
@@ -20,7 +33,8 @@ fn main() {
 
         let type_name = pattern.type_name();
         let filename = format!(
-            "output_{}.png",
+            "./{}/output_{}.png",
+            OUTPUT_DIR,
             type_name
                 .replace("sample_image_generator::", "")
                 .replace("::", "_")
