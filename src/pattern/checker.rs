7e9e6b7ff70d1648c82850eea55e6045fe906298
BlueGeckoJP <kei09doya@gmail.com>
2025-12-12 10:03:21 +0900

    fix: fix incorrect checker pattern implementation

--- src/pattern/checker.rs
+++ src/pattern/checker.rs
@@ -4,6 +4,12 @@ pub struct CheckerPattern {}
 
 impl Pattern for CheckerPattern {
     fn generate(&self, width: u32, height: u32) -> Vec<u8> {
-        (0..(width * height)).map(|i| (i % 2) as u8).collect()
+        (0..(width * height))
+            .map(|i| {
+                let bit = (i % 2) as u8;
+                let invert = ((i / width) & 1) == 0;
+                if invert { 1 - bit } else { bit }
+            })
+            .collect()
     }
 }
