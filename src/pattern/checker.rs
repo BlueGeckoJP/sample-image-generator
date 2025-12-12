a1182ae1d28a33f1ecc973f6462aedb8cce44876
BlueGeckoJP <kei09doya@gmail.com>
2025-12-12 10:08:04 +0900

    perf: update bit calculation logic to bitwise op

--- src/pattern/checker.rs
+++ src/pattern/checker.rs
@@ -6,7 +6,7 @@ impl Pattern for CheckerPattern {
     fn generate(&self, width: u32, height: u32) -> Vec<u8> {
         (0..(width * height))
             .map(|i| {
-                let bit = (i % 2) as u8;
+                let bit = (i & 1) as u8;
                 let invert = ((i / width) & 1) == 0;
                 if invert { 1 - bit } else { bit }
             })
